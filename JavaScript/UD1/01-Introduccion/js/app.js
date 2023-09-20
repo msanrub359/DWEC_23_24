8b431b1b55d57a14d6a447b04969ac1aad7873a2
msanrub359 <msanrub359@gmail.com>
2023-09-20 11:24:14 +0200

    UD1- clase miércoles

--- JavaScript/UD1/01-Introduccion/js/app.js
+++ JavaScript/UD1/01-Introduccion/js/app.js
@@ -1,2 +1,10 @@
-'use strict';
-console.log('hola Mundo JavaScript');
+
+console.log('hola Mundo JavaScript');//mostrar datos en consola
+// alert("Buenos días, a 'JavaScript'"); //mostrar dato en ventana del navegador
+//let respuesta=confirm("¿Desea salir del navegador");
+// if (confirm("¿Desea salir del navegador")===true){
+//     alert("Ha pulsado aceptar")
+// }else{
+//     alert("Ha pulsado cancelar")
+// }
+console.log(typeof prompt('Introduzca edad',0));
