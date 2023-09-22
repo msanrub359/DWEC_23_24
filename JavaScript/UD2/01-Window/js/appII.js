823203209cfc0e5ab141fc062ff4cc7059da24a6
msanrub359 <msanrub359@gmail.com>
2023-09-22 12:31:14 +0200

    UD2

---
+++ JavaScript/UD2/01-Window/js/appII.js
@@ -0,0 +1,22 @@
+"use strict";
+//permitir crear solo una ventana secundaria y no se
+//cerrará la ventana principal, si ventana secundaria
+//está abierta
+let ventana;
+//abrir subventana
+function crearVent() {
+  if (ventana==undefined || ventana.closed) {//devuelve true si está cerrada y false se está abierta
+    ventana = window.open("", "secundaria", "width=200,height=200");
+    //añadir título y botón a la ventana secundaria
+    ventana.document.write("<h1>Ventana secundaria</h1>");
+    ventana.document.write(
+      "<button type='button' onclick='self.close()'>Cerrar ventana</button>"
+    );
+  }else{
+    alert("La ventana secundaria ya está abierta")
+  }
+}
+
+function cerrarVent() {
+  self.close();
+}
