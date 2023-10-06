778a1259d8e127a8d22d2590e6585aedea74c5f1
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2023-10-06 11:03:18 +0200

    Add files via upload

---
+++ JavaScript/UD3/06-Mapas/js/appEjercicio.js
@@ -0,0 +1,15 @@
+"use strict"
+/**
+ * @description Crear un script que utilice la estructura de map para
+ * realizar el siguimiento de las puntuaciones de los jugadores. 
+ * Se pedirá mediante prompt el nombre y la puntuación.
+ * El nombre no podrá ser vacío y al pulsar el botón Cancelar,
+ * finalizará la entrada de datos (función, devuelven el dato)
+ * Las puntuaciones, se controlarán que sean numéricas y no se permitirán
+ * datos nulos (función, devuelven el dato)
+ * A continuación, se ordenará por puntuación ascendente y se mostrará
+ * el mapa ordenado.
+ * Utilizar funciones para la entrada de datos, otra para la ordenación
+ * y otra para mostrar el mapa ordenado.
+ */
+
