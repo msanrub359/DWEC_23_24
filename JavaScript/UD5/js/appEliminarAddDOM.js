2740d00e5ba7191079ce8c4bd4de1d593625f733
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2023-11-09 13:37:55 +0100

    Add files via upload

--- JavaScript/UD5/js/appEliminarAddDOM.js
+++ JavaScript/UD5/js/appEliminarAddDOM.js
@@ -1,4 +1,6 @@
 "use strict"
+//import Swal from 'sweetalert2'
+// import '../../assets/librerias/sweetalert2.min.css'
 
 const evento=()=>{
   Swal.fire({
@@ -19,13 +21,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
   const navegacion=document.querySelector(".navegacion");
   console.log(navegacion);
   //acceder al primer elemento del menú
-  //  console.log(navegacion.firstElementChild);
-  //  navegacion.firstElementChild.remove();
-  //  console.log(navegacion.children);
-  //  navegacion.children[0].remove()
+  // console.log(navegacion.firstElementChild);
+  // navegacion.firstElementChild.remove();
+    // console.log(navegacion.children);
+    // navegacion.children[0].remove()
 
 //Eliminar el nodo desde el padre
-// navegacion.removeChild(navegacion.children[1]);
+ //navegacion.removeChild(navegacion.children[0]);
 
 //crear un enlace
 
@@ -34,7 +36,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
   enlace.setAttribute("href", '#');
   enlace.addEventListener("click", evento)
 // añadir al menú al final
-  // navegacion.appendChild(enlace);
+  //  navegacion.appendChild(enlace);
 // //añadir entre otros nodos
  navegacion.insertBefore(enlace, navegacion.children[1]);
 })
