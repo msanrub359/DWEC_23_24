8b911b88c9f7e8fff59fbe8bb07b5071cd660702
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2023-12-21 12:09:43 +0100

    Update API.js

--- JavaScript/UD7/CRUD/js/API.js
+++ JavaScript/UD7/CRUD/js/API.js
@@ -1,1 +1,35 @@
+
 "use strict"
+ const url="http://localhost:3000/clientes"
+
+ export const addCliente=async (cliente)=>{
+
+    const param={
+        method:"POST",
+        body:cliente, //nomCliente="pepe"&emailCliente="dkldl@gmail.com"
+        headers:{
+            'Content-Type': "application/x-www-form-urlencoded"
+        }
+    }
+    try {
+        const response= await fetch(url, param);
+        const data= await response.json();
+        return ({'mensaje': 'insertado'}) 
+    } catch (error) {
+        return ({'mensaje': 'NO insertado'}) 
+    }
+    
+
+ }
+ export const getClientes=async ()=>{
+
+        try {
+        const response= await fetch(url);
+        const data= await response.json();
+        return ({'data': data}) 
+    } catch (error) {
+        return ({'error': error}) 
+    }
+    
+
+ }
