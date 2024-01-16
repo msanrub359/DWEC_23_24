d261e96f9636b07037559c7c01d8fce3b52d50e1
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2024-01-16 16:25:25 +0100

    Update clientes.routes.js

--- JavaScript/UD10/09_servidorHttpMySQLControllersValidators/src/routes/clientes.routes.js
+++ JavaScript/UD10/09_servidorHttpMySQLControllersValidators/src/routes/clientes.routes.js
@@ -1,13 +1,14 @@
 "use strict"
 import {Router} from 'express'
 import { getClientes,  getCliente, delCliente,addCliente, updateCliente } from '../controllers/clientes.controllers.js';
+import { validacion } from '../validators/clientes.validators.js';
 
 const router=Router();
 
 router.get("/clientes", getClientes)
 router.get("/clientes/:id", getCliente)
-router.post("/clientes", addCliente);
-router.put("/clientes/:id", updateCliente);
+router.post("/clientes", validacion, addCliente);
+router.put("/clientes/:id", validacion, updateCliente);
 router.patch("/clientes/:id", updateCliente);
 router.delete("/clientes/:id", delCliente )
 
